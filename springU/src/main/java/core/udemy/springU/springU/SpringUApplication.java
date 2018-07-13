7c7272f8e84255523541f2f06b6ed40e407b8712
mario quiroz <mario.quiroz@atomic32.com>
2018-07-13 17:39:42 -0500

    Primer commit en mi repositorio

---
+++ springU/src/main/java/core/udemy/springU/springU/SpringUApplication.java
@@ -0,0 +1,12 @@
+package core.udemy.springU.springU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class SpringUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUApplication.class, args);
+	}
+}
